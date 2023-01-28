637c03cb095edd361de940797de8ca77778ea347
Bilal Softic <bilalsoftic99@gmail.com>
2023-01-28 19:08:17 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// ===variables===
+const artBtns = document.querySelectorAll(".art-btn");
+
+// ===dropdown text===
+artBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const artContainer = e.currentTarget.parentElement.parentElement;
+    artContainer.classList.toggle("show-text");
+    console.log(btn);
+  });
+});
+
+// ===fixed navbar===
+// if navbar height changes the condition also changes
+window.addEventListener("scroll", function (params) {
+  const navbar = document.querySelector("nav");
+  // const offset = navbar.offsetTop;
+  // console.log(offset);
+  if (window.scrollY > 143) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
