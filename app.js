9a570688c3b8f902c22b5c473d92c385dbcc8239
Bilal Softic <bilalsoftic99@gmail.com>
2023-02-03 12:52:52 +0100

    article display function

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const doctors = [
   {
     id: 1,
-    img: "./image/doctor-images/first-doctor",
+    img: "./image/doctor-images/first-doctor.jpg",
     header: "the 1st doctor",
     actor: "William Hartnell",
     years: "1963-1966",
     series: "4",
+    text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nostrum veniam eaque nobis laborum quisquam, sunt perspiciatis illo sed adipisci aspernatur vero earum dolorem ex voluptas excepturi eligendi corrupti dolorum, sint accusantium. Enim, necessitatibus dignissimos consequatur explicabo quae nobis similique beatae ullam velit earum quaerat unde fugiat, eveniet, tempora quibusdam!",
   },
   {
     id: 2,
-    img: "./image/doctor-images/second-doctor",
+    img: "./image/doctor-images/second-doctor.jpg",
     header: "the 2nd doctor",
     actor: " Patrick Troughton",
     years: "1966-1969",
     series: "3",
+    text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nostrum veniam eaque nobis laborum quisquam, sunt perspiciatis illo sed adipisci aspernatur vero earum dolorem ex voluptas excepturi eligendi corrupti dolorum, sint accusantium. Enim, necessitatibus dignissimos consequatur explicabo quae nobis similique beatae ullam velit earum quaerat unde fugiat, eveniet, tempora quibusdam!",
   },
 ];
 
 // ===variables===
-const artBtns = document.querySelectorAll(".art-btn");
 const bars = document.querySelector(".bars-icon");
 const navbar = document.querySelector(".nav");
-// ===dropdown text===
-artBtns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const artContainer = e.currentTarget.parentElement.parentElement;
-    artContainer.classList.toggle("show-text");
-    console.log(btn);
-  });
+const displayContainer = document.querySelector(".container-center");
+
+// ===display article elements===
+window.addEventListener("DOMContentLoaded", function () {
+  displayArticles(doctors);
+  dropdownMenu();
 });
 
 // ===fixed navbar===
@@ -42,12 +42,55 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("fixed");
   }
 });
-
 // ===dropdown menu===
+function dropdownMenu() {
+  bars.addEventListener("click", function () {
+    navbar.classList.toggle("show-list");
+  });
+  // ===dropdown text===
+  const artBtns = document.querySelectorAll(".art-btn");
+  artBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const artContainer = e.currentTarget.parentElement.parentElement;
+      artContainer.classList.toggle("show-text");
+      console.log(btn);
+    });
+  });
+}
+// ===display articles===
+function displayArticles(arr) {
+  const displayItems = arr
+    .map(function (item) {
+      return `<div class="art-container" id="1">
+    <img
+    src=${item.img}
+    alt="doctor-image"
+    class="art-image"
+    />
+    <div class="underline"></div>
+    <div class="art-header">
+    <h1>${item.header}</h1>
+    <button class="art-btn">
+    <span class="down-icon"
+    ><i class="fa-solid fa-chevron-down"></i
+    ></span>
+  <span class="up-icon"
+  ><i class="fa-solid fa-chevron-up"></i
+      ></span>
+      </button>
+      </div>
+  <div class="art-content">
+  <h2 class="actor-name">${item.actor}</h2>
+  <h2 class="time">${item.years}</h2>
+  <h2>no. of series: <span id="series-nmbr">${item.series}</span></h2>
+  <div class="underline"></div>
+  <p>
+  ${item.text}
+  </p>
+  </div>
+  </div>`;
+    })
+    .join("");
 
-bars.addEventListener("click", function () {
-  navbar.classList.toggle("show-list");
-});
-
-// ===display article elements===
-window.addEventListener("DOMContentLoaded", function () {});
+  displayContainer.innerHTML = displayItems;
+}
